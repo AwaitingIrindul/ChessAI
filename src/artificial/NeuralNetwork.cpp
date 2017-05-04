f778d431c91d02de09da9ec0c05f571650295e0f
Irindul <mathieu.regnard2@gmail.com>
2017-05-04 21:21:31 +0200

    Created artificial neural network class

---
+++ src/artificial/NeuralNetwork.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Mathieu Regnard on 04/05/2017.
+//
+
+#include "NeuralNetwork.h"
---
+++ src/artificial/NeuralNetwork.h
@@ -0,0 +1,14 @@
+//
+// Created by Mathieu Regnard on 04/05/2017.
+//
+
+#ifndef CHESSPLUSPLUS_NEURALNETWORK_H
+#define CHESSPLUSPLUS_NEURALNETWORK_H
+
+
+class NeuralNetwork {
+
+};
+
+
+#endif //CHESSPLUSPLUS_NEURALNETWORK_H
